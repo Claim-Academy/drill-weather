f277bc67c9153270c251cd68973b29b8f04fab7c
Manav Misra <manavm@visionwebsoft.com>
2023-01-09 11:52:36 -0600

    Have API just send back the index 0 element

--- app/api.service.js
+++ app/api.service.js
@@ -3,7 +3,11 @@ import got from "got";
 const BASE_URL = "http://localhost:3000/weather";
 
 export default {
-  getWeather() {
-    return got(BASE_URL).json();
+  async getWeather() {
+    // Get the weather data array
+    const weatherData = await got(BASE_URL).json();
+
+    // Send back index 0 of the array
+    return weatherData[0];
   },
 };
